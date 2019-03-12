63606d42106e5e2d1b0a124306c6f99acc5e6d3d
Philip Wisner <philipwisner@gmail.com>
2019-03-12 08:56:23 -0400

    added firebase call

--- src/app/shared/orders.service.ts
+++ src/app/shared/orders.service.ts
@@ -8,11 +8,19 @@ import { AngularFirestore } from "@angular/fire/firestore";
 })
 export class OrdersService {
 
-  constructor(private fireStore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore) { }
   form = new FormGroup({
     customerName: new FormControl(''),
     orderNumber: new FormControl(''),
     coffeeOrder: new FormControl(''),
     completed: new FormControl(false)
   })
+  createCoffeeOrder(data) {
+    return new Promise<any>((resolve, reject) => {
+      this.firestore
+        .collection("coffeeOrders")
+        .add(data)
+        .then(res => { }, err => reject(err));
+    });
+  }
 }

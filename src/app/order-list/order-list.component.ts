a3bf3b41dcdbe69d61e7f6204855255cb0bdf09d
Philip Wisner <philipwisner@gmail.com>
2019-03-13 09:07:19 -0400

    added function in comp

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../shared/orders.service';
 
 @Component({
   selector: 'app-order-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) { }
+  coffeeOrders;
 
   ngOnInit() {
+    this.getCoffeeOrders();
   }
 
+  getCoffeeOrders = () => this.ordersService.getCoffeeOrders().subscribe(res = (this.coffeeOrders = res));
 }

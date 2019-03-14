3458a782486225a940cc8690195a94e244b5c9c2
Philip Wisner <philipwisner@gmail.com>
2019-03-14 11:59:07 -0400

    added delete call

--- src/app/shared/orders.service.ts
+++ src/app/shared/orders.service.ts
@@ -37,4 +37,11 @@ export class OrdersService {
       .doc(data.payload.doc.id)
       .set({ completed: true }, { merge: true });
   }
+
+  deleteCoffeeOrder(data) {
+    return this.firestore
+      .collection("coffeeOrders")
+      .doc(data.payload.doc.id)
+      .delete();
+  }
 }
